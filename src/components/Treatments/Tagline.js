200e2a3939e62a93e466d9486d48d50695ab5639
stevebarakat <stevebarakat@gmail.com>
2020-01-16 05:21:05 -0500

    added icons again

---
+++ src/components/Treatments/Tagline.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Treatments.css'
+
+
+const Tagline = () => (
+  <>
+              <div className="section-header">
+                <h3>Injuries &amp; Conditions We Treat</h3>
+                We diagnose and treat pain related to work, sports, auto-accidents, and more.
+              </div>
+              <div className="treatments-list">
+                <ul>
+                  <li>Neck and low back pain</li>
+                  <li>Auto accident-related injuries</li>
+                  <li>Tendinitis of all kinds</li>
+                  <li>Headaches</li>
+                  <li>Tingling in arms and legs</li>
+                  <li>Radiating Pain</li>
+                  <li>Shoulder injuries</li>
+                  <li>Numbness</li>
+                </ul>
+                <ul>
+                  <li>Disc Injury</li>
+                  <li>Sports related injuries</li>
+                  <li>Sciatica</li>
+                  <li>Muscle tightness</li>
+                  <li>Back disc related issues</li>
+                  <li>Whiplash</li>
+                  <li>Digestive issues</li>
+                  <li>Senior Car Accident Care</li>
+                </ul>
+              </div>
+            </>
+)
+
+export default Tagline
