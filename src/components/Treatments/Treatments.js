7466319277948a385b4ecd10b3b07b32b71c3dd5
stevebarakat <stevebarakat@gmail.com>
2020-03-30 05:27:09 -0400

    fixed stuff

--- src/components/Treatments/Treatments.js
+++ src/components/Treatments/Treatments.js
@@ -1,85 +1,107 @@
 import React from 'react'
 import { useState, useEffect } from "react";
-import { motion, useViewportScroll } from "framer-motion";
+import { motion, useAnimation, positionTransition } from "framer-motion";
 import { shuffle } from "lodash";
 import { useIntersectionObserver } from 'react-intersection-observer-hook';
 import { useStaticQuery, graphql } from "gatsby"
 import Typed from "react-typed";
 import styles from "./treatments.module.css"
 
-const spring = {
-  type: "spring",
-  damping: 12,
-  stiffness: 30,
-  mass: 0.5
-};
-
-
-
 const Treatments = () => {
-
+  const controls = useAnimation()
   const [ref, { entry }] = useIntersectionObserver();
-  const isVisible = entry && entry.isIntersecting;
+  const isCrossing = entry && entry.isIntersecting;
+  const list = {
+    positionTransition,
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.3,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      transition: {
+        when: "afterChildren",
+      },
+    },
+  }
+
+  const item = {
+    visible: { opacity: 1, x: 0, rotate: 0 },
+    hidden: { opacity: 0, x: -100, rotate: 180 },
+  }
 
   const data = useStaticQuery(graphql`
-    {
-      allWordpressAcfPages(filter: {wordpress_id: {eq: 523}}) {
-        nodes {
-          acf {
-            treatment {
-              treatment_name
-              treatment_image {
-                source_url
+  {
+    allWordpressAcfPages(filter: {wordpress_id: {eq: 523}}) {
+      nodes {
+        acf {
+          treatment {
+            treatment_name
+            treatment_image {
+              localFile {
+                childImageSharp {
+                  fluid {
+                    src
+                  }
+                }
               }
             }
           }
         }
       }
     }
+  }
   `)
 
-  
+
   const treatmentsQuery = data.allWordpressAcfPages.nodes[0].acf;
-    useEffect(() => {
-      isVisible ? console.log("is visible") : console.log("not visible")
-    }, [isVisible])
+
+  console.log(treatmentsQuery.treatment[5].treatment_image.localFile.childImageSharp.fluid.src)
+  let listItems = treatmentsQuery.treatment.map((treatment, i) => {
+    return (
+      <motion.li
+        custom={i}
+        variants={item}
+        animate={controls}
+      >
+        <img src={treatment.treatment_image.localFile.childImageSharp.fluid.src} />
+        <span>{treatment.treatment_name}</span>
+      </motion.li>
+    )
+  })
+
   return (
     <section id="treatments" className={`nfcpt-bg-dark ${styles.treatments}`}>
       <div className="section-header">
         <h3><Typed
-        strings={[
-          "CHIROPRACTIC CARE MAY HELP WITH...",
-          "AUTO ACCIDENT INJURIES",
-          "NECK AND BACK PAIN",
-          "SCIATICA",
-          "CARPEL TUNNEL",
-          "HEADACHES",
-          "SLIPPED DISC",
-          "WHIPLASH AND MORE..."
-        ]}
-        typeSpeed={100}
-        backSpeed={55}
-        loop
-      /></h3>
-          We diagnose and treat pain related to work, sports, auto-accidents, and more.
-              </div>
+          strings={[
+            "CHIROPRACTIC CARE MAY HELP WITH...",
+            "AUTO ACCIDENT INJURIES",
+            "NECK AND BACK PAIN",
+            "SCIATICA",
+            "CARPEL TUNNEL",
+            "HEADACHES",
+            "SLIPPED DISC",
+            "WHIPLASH AND MORE..."
+          ]}
+          typeSpeed={100}
+          backSpeed={55}
+          loop
+        /></h3>
+        <p>We diagnose and treat pain related to work, sports, auto-accidents, and more.</p>
+      </div>
       <div className="treatments-list">
 
         <motion.ul
           ref={ref}
+          initial="hidden"
+          animate={isCrossing ? "visible" : "hidden"}
+          variants={list}
         >
-          {treatmentsQuery.treatment.map((treatment, i) => {
-            return (
-              <motion.li
-                initial={{ x: -500 }}
-                animate={{ x: 0 }}
-                transition={{ duration: 5 }}
-              >
-                <img src={treatment.treatment_image.source_url} />
-                <span>{treatment.treatment_name}</span>
-              </motion.li>
-            )
-          })}
+          {listItems}
         </motion.ul>
       </div>
     </section>
